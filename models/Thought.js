5fe16e440312e06256b0367b55d74e37678cdd8d
FranklinBrad <franklin1jbradley@gmail.com>
2023-11-29 16:30:59 -0600

    error testing

--- models/Thought.js
+++ models/Thought.js
@@ -1,32 +1,9 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const validator = require("validator");
-
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    maxlength: 280,
-    minlength: 1,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  userName: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
-
-  toJSON: {
-    virtuals: true,
-  },
-  id: false,
-});
 
 const reactionSchema = new Schema({
   reactionId: {
-    type: Schema.Types.ObjectId(),
+    type: Schema.Types.ObjectId,
     default: () => new Types.ObjectId(),
   },
   reactionBody: {
@@ -43,11 +20,43 @@ const reactionSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-});
-ThoughtSchema.virtual("reactionCount").get(function () {
-  return this.reaction.length;
+  toJSON: {
+    getters: true,
+  },
+  id: false,
 });
 
-const Thought = model("thought", thoughtSchema);
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
